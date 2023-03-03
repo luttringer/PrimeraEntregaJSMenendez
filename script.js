2bf2e46defa3a9d3698a6bfe985526dc8b4ce75d
Christian Menendez Luttringer <luttringerezequiel@gmail.com>
2023-03-03 01:09:32 -0300

    se agregan primeras instrucciones en js y fondo

---
+++ script.js
@@ -0,0 +1,9 @@
+let userAnswer;
+
+do
+{
+    userAnswer = prompt("Bienvenid@ a mi, un simulador de viajes espaciales. Serás guiad@ mediante instrucciones y entradas de datos como: \"velocidad de viaje\" y \"destino\" hacia un estimado de tu tiempo de viaje ademas de recibir diferentes características útiles de tu destino final siendo este algun planeta del Sistema Solar o alguna luna de gran tamaño. Todos los viajes asumirán que partes desde la Tierra. Si has entendido escribe la palabra \"entiendo\".").toLowerCase();
+}while(userAnswer!="entiendo");
+
+
+alert("genial");
